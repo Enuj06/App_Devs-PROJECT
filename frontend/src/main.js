3aed1637f640df33f1d3c5d4fa9f5bf6b3fe6874
Enuj06 <junebalaibo03@gmail.com>
2023-11-17 21:06:07 +0800

    login

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import axios from 'axios'
+import './assets/css/main.css'
 
 axios.defaults.baseURL="http://App_Devs-PROJECT-backend.test"
 
--- frontend/src/views/IndexPage.vue
+++ frontend/src/views/IndexPage.vue
@@ -1,5 +1,17 @@
 <template>
     <div>
+        <h1>Welcome to MinSU Guidance Office!</h1>
+
+        <form class="form">
+            <h1 style="text-align: center">Login</h1>
+            <input type="text" id="username" class="form-control" name="username" placeholder="Username"><br>
+            <input type="text" id="password" class="form-control" name="passoword" placeholder="Password"><br>
+        <button type="submit" class="btn">Login</button> 
+        <h3>Don't have an account?
+        <a href="create_account">Create Account</a></h3>
+    </form>
+    </div>
+    <div>
         <table border="1">
             <tr>
                 <th>LAST NAME</th>
@@ -16,6 +28,10 @@
         </table>
     </div>
 </template>
+
+<style scoped>
+
+ </style>
 
     <script>
         import axios from 'axios'
